82452c0a80995324e78ca8ab8856b435d74e00e3
Jestzer <james@buonopane.email>
2024-04-05 20:38:56 -0400

    Added gitignore. Basically finished it and it seems to largely work.

--- mw-service-host-troubleshooter.go
+++ mw-service-host-troubleshooter.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"archive/zip"
 	"fmt"
 	"io"
 	"net/http"
@@ -10,6 +10,7 @@ import (
 	"os/signal"
 	"os/user"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"syscall"
 
@@ -23,6 +24,8 @@ func main() {
 	redText := color.New(color.FgRed).SprintFunc()
 	cyanText := color.New(color.FgCyan).SprintFunc()
 	redBackground := color.New(color.BgRed).SprintFunc()
+	var serviceHostFolderExists bool
+	var matlabConnectorFolderExists bool
 
 	// Use readline to make text input not suck so much.
 	rl, err := readline.New("> ")
@@ -65,7 +68,8 @@ func main() {
 	// Get a list of all running processes.
 	processes, err := process.Processes()
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		errorMsg := redText(fmt.Sprintf("Error: %v\n", err))
+		fmt.Print(errorMsg)
 		os.Exit(1)
 	}
 
@@ -77,21 +81,39 @@ func main() {
 			fmt.Printf("Process '%s' is running with PID %d\n", name, p.Pid)
 
 			// Prompt the user to end the process.
-			fmt.Print("Do you want to end this process? (y/n)\n>> ")
-			reader := bufio.NewReader(os.Stdin)
-			input, _ := reader.ReadString('\n')
-			input = strings.TrimSpace(input)
+			fmt.Print("Do you want to end this process? (y/n)\n")
 
-			if strings.EqualFold(input, "y") {
-				err := p.Terminate()
-				if err == nil {
-					fmt.Printf("%s (PID %d) has been terminated.\nPlease wait for the next step.", name, p.Pid)
-				} else {
-					fmt.Printf("Error terminating the process: %v\n", err)
+			for {
+				input, err := rl.Readline()
+				if err != nil {
+					if err.Error() == "Interrupt" {
+						fmt.Println(redText("Exiting from user input."))
+					} else {
+						fmt.Print(redText("Error reading line: ", err))
+						continue
+					}
+					return
 				}
-			} else {
-				fmt.Println("Process was not terminated. Exiting the program until you're ready.")
-				os.Exit(0)
+				input = strings.TrimSpace(input)
+				input = strings.ToLower(input)
+
+				if input == "y" || input == "yes" {
+					err := p.Terminate()
+					if err == nil {
+						fmt.Printf("%s (PID %d) has been terminated.\nPlease wait for the next step.", name, p.Pid)
+					} else {
+						errorMsg := redText(fmt.Sprintf("Error terminating the process: %v\n", err))
+						fmt.Print(errorMsg)
+						os.Exit(1)
+					}
+					break
+				} else if input == "n" || input == "no" {
+					fmt.Println(redText("Exiting since the user has declined moving necessary folders."))
+					os.Exit(0)
+				} else {
+					fmt.Println(redText("Invalid input. Type in \"y\" or \"n\""))
+					continue
+				}
 			}
 			break
 		}
@@ -104,7 +126,8 @@ func main() {
 	// Get the Windows username from whoever launched the program.
 	currentUser, err := user.Current()
 	if err != nil {
-		fmt.Printf("Error detecting the Windows username: %v\n", err)
+		errorMsg := redText(fmt.Sprintf("Error detecting the Windows username: %v\n", err))
+		fmt.Print(errorMsg)
 		os.Exit(1)
 	}
 	username := extractUsername(currentUser.Username)
@@ -116,98 +139,175 @@ func main() {
 	matlabConnectorFolder := fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\MathWorks\\MATLABConnector", username)
 
 	// Define the new Service Host download directory.
-	newServiceHostDir := fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Temp", username)
+	newServiceHostDownloadFolder := fmt.Sprintf("C:\\Users\\%s\\AppData\\Local\\Temp", username)
 
+	// Define where the old, moved Service Host folders will go.
 	movedServiceHostFilesFolder := filepath.Join(thisProgramDir, "Old_service_host_files")
 
-	// Ask the user if they are ready to move the old ServiceHost folders
-	fmt.Println("\nAre you ready to move the old ServiceHost folder, MATLAB Connector folder, their contents, and all other related files?")
-	fmt.Print(cyanText("Any related content and folders marked for being moved above will be moved into a folder called \"Old_service_host_files\""))
-	fmt.Print(cyanText(". You are responsible for deleting this folder (if you wish) after the old Service Host files are moved into this folder."))
-	fmt.Print(cyanText("\nType \"move\" to confirm. This is case-sensitive."))
-	fmt.Print(cyanText("Type any anything else to cancel.\n"))
+	// Define where the Service Host extracted installation files will go.
+	extractedServiceHostInstaller := filepath.Join(newServiceHostDownloadFolder, "extractedMWSInstaller")
 
-	for {
-		input, err := rl.Readline()
-		if err != nil {
-			if err.Error() == "Interrupt" {
-				fmt.Println(redText("Exiting from user input."))
-			} else {
-				fmt.Print(redText("Error reading line:", err))
-				continue
+	// Check if the existing Service Host folder exists.
+	if _, err := os.Stat(serviceHostFolder); err == nil {
+		serviceHostFolderExists = true
+	} else {
+		serviceHostFolderExists = false
+	}
+
+	// Check if the existing MATLAB Connector folder exists.
+	if _, err := os.Stat(matlabConnectorFolder); err == nil {
+		matlabConnectorFolderExists = true
+	} else {
+		matlabConnectorFolderExists = false
+	}
+
+	if serviceHostFolderExists || matlabConnectorFolderExists {
+		// Ask the user if they are ready to move the old ServiceHost folders.
+		fmt.Println("\nAre you ready to move the old ServiceHost folder, MATLAB Connector folder, their contents, and all other related files?")
+		fmt.Print(cyanText("Any related content and folders marked for being moved above will be moved into a folder called \"Old_service_host_files\"\n"))
+		fmt.Print(cyanText(". You are responsible for deleting this folder (if you wish) after the old Service Host files are moved into this folder."))
+		fmt.Print(cyanText("\nType \"move\" to confirm. This is case-sensitive. Type any anything else to cancel.\n"))
+
+		for {
+			input, err := rl.Readline()
+			if err != nil {
+				if err.Error() == "Interrupt" {
+					fmt.Println(redText("Exiting from user input."))
+				} else {
+					fmt.Print(redText("Error reading line: ", err))
+					continue
+				}
+				return
 			}
-			return
-		}
-		input = strings.TrimSpace(input)
+			input = strings.TrimSpace(input)
 
-		if input == "move" {
-			break
+			if input == "move" {
+				break
+			} else {
+				fmt.Println(redText("Exiting since the user has declined moving necessary folders."))
+				os.Exit(0)
+			}
+		}
+
+		// Create the junk folder, if it doesn't already exist. Add a number to the end of the folder if you've already tried this.
+		// However, if it's failing after 25 times, just assume something is wrong.
+		i := 1
+		for {
+			movedServiceHostFilesFolder = filepath.Join(thisProgramDir, "Old_service_host_files")
+			if i > 1 {
+				movedServiceHostFilesFolder = fmt.Sprintf("%s%d", movedServiceHostFilesFolder, i)
+			}
+
+			if _, err := os.Stat(movedServiceHostFilesFolder); os.IsNotExist(err) {
+				// If the folder does not exist, try to create it.
+				if err := os.MkdirAll(movedServiceHostFilesFolder, 0755); err != nil {
+					fmt.Println(redText("Error creating directory: ", err))
+					os.Exit(1)
+				} else {
+					fmt.Println("Directory created successfully:", movedServiceHostFilesFolder)
+					break
+				}
+			} else if i >= 26 {
+				// If the folder exists and we've reached the maximum attempt count, exit with an error.
+				fmt.Println(redText("Error: Reached maximum directory creation attempts."))
+				os.Exit(1)
+			}
+
+			i++
+		}
+
+		if serviceHostFolderExists {
+			// Move the ServiceHost folder.
+			err := moveFolder(serviceHostFolder, filepath.Join(movedServiceHostFilesFolder, "ServiceHost"))
+			if err != nil {
+				errorMsg := redText(fmt.Sprintf("Error moving ServiceHost folder: %s\n", err))
+				fmt.Print(errorMsg)
+				os.Exit(1)
+			} else {
+				fmt.Println("ServiceHost folder moved successfully.")
+			}
+		}
+	}
+
+	if matlabConnectorFolderExists {
+		// Move the MATLABConnector folder.
+		err = moveFolder(matlabConnectorFolder, filepath.Join(movedServiceHostFilesFolder, "MATLABConnector"))
+		if err != nil {
+			errorMsg := redText(fmt.Sprintf("Error moving MATLABConnector folder: %s\n", err))
+			fmt.Print(errorMsg)
+			os.Exit(1)
 		} else {
-			fmt.Println(redText("Exiting since the user has declined moving necessary folders."))
-			os.Exit(0)
+			fmt.Println("MATLABConnector folder moved successfully.")
 		}
-	}
-
-	// Move the ServiceHost folder.
-	err = moveFolder(serviceHostFolder, filepath.Join(movedServiceHostFilesFolder, "ServiceHost"))
-	if err != nil {
-		fmt.Printf(redText("Error moving ServiceHost folder: %s\n", err))
-		os.Exit(1)
-	} else {
-		fmt.Println("ServiceHost folder moved successfully.")
-	}
-
-	// Move the MATLABConnector folder.
-	err = moveFolder(matlabConnectorFolder, filepath.Join(movedServiceHostFilesFolder, "MATLABConnector"))
-	if err != nil {
-		fmt.Printf(redText("Error moving MATLABConnector folder: %s\n", err))
-		os.Exit(1)
-	} else {
-		fmt.Println("MATLABConnector folder moved successfully.")
 	}
 
 	fmt.Println("Downloading latest Service Host installer. Please wait.")
 
 	installerURL := "https://ssd.mathworks.com/supportfiles/downloads/MathWorksServiceHost/v2024.2.0.3/installers/mathworksservicehost_2024.2.0.3_win64_installer.exe"
-	installerPath := filepath.Join(newServiceHostDir, "mathworksservicehost_installer.exe")
+	installerPath := filepath.Join(newServiceHostDownloadFolder, "mathworksservicehost_installer.exe")
 
-	// Create the destination directory ,if it doesn't already exist.
-	if err := os.MkdirAll(newServiceHostDir, 0755); err != nil {
-		fmt.Println(redText("Error creating directory:", err))
-		os.Exit(1)
+	// Check if the downloaeded Service Host WinZip executable already exists. Delete it if it is.
+	if _, err := os.Stat(installerPath); err == nil {
+		err := os.Remove(installerPath)
+		if err != nil {
+			fmt.Print(redText("\nFailed to delete the existing Service Host installer directory: ", err))
+			os.Exit(1)
+		}
 	}
 
 	// Download the installer and save it.
 	resp, err := http.Get(installerURL)
 	if err != nil {
-		fmt.Println(redText("Error downloading installer:", err))
+		fmt.Println(redText("Error downloading installer: ", err))
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
 
 	file, err := os.Create(installerPath)
 	if err != nil {
-		fmt.Println(redText("Error creating installer file:", err))
+		fmt.Println(redText("Error creating installer file: ", err))
 		os.Exit(1)
 	}
 	defer file.Close()
 
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
-		fmt.Println(redText("Error saving installer:", err))
+		fmt.Println(redText("Error saving installer: ", err))
 		os.Exit(1)
 	}
 
+	// Check if the extracted Service Host installer directory already exists. Delete it if it is.
+	if _, err := os.Stat(extractedServiceHostInstaller); err == nil {
+		err := os.RemoveAll(extractedServiceHostInstaller)
+		if err != nil {
+			fmt.Print(redText("\nFailed to delete the existing Service Host installer directory: ", err))
+			os.Exit(1)
+		}
+	}
+
+	// Extract the WinZIP archive.
+	err = unzipFile(installerPath, extractedServiceHostInstaller)
+	if err != nil {
+		fmt.Print(redText("\nFailed to extract Service Host installer: ", err))
+		os.Exit(1)
+	}
+
+	extractedInstallerPath := filepath.Join(extractedServiceHostInstaller, "bin\\win64\\InstallMathWorksServiceHost.exe")
+
 	// Run the installer
-	cmd := exec.Command(installerPath)
+	cmd := exec.Command(extractedInstallerPath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	err = cmd.Run()
 	if err != nil {
-		// Add some code to extract the installer, rather than just running it, since that doesn't seem to work.
-		fmt.Println(redText("Error running the installer:", err))
-		os.Exit(1)
+		if strings.Contains(err.Error(), "exit status 66") {
+			fmt.Println(cyanText("Installation started! Please wait.\n"))
+			fmt.Print(cyanText("When you are prompted to \"Setup MATLAB Drive Connector\", you may then select\"Exit\" and then \"OK\""))
+		} else {
+			fmt.Println(redText("Error running the installer: ", err))
+			os.Exit(1)
+		}
 	} else {
 		fmt.Println("Installation completed! Please attempt to launch MATLAB.")
 	}
@@ -231,23 +331,49 @@ func extractUsername(fullUsername string) string {
 	return fullUsername
 }
 
-// Function to detect folder size.
-func getFolderSize(folderPath string) (int64, error) {
-	var folderSize int64
+// Function to unzip integration scripts.
+func unzipFile(src, dest string) error {
+	reader, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
 
-	err := filepath.Walk(folderPath, func(path string, info os.FileInfo, err error) error {
+	for _, file := range reader.File {
+		path := filepath.Join(dest, file.Name)
+
+		// Reconstruct the file path on Windows to ensure proper subdirectories are created. Don't know why other OSes don't need this.
+		if runtime.GOOS == "windows" {
+			path = filepath.Join(dest, file.Name)
+			path = filepath.FromSlash(path)
+		}
+
+		if file.FileInfo().IsDir() {
+			os.MkdirAll(path, file.Mode())
+			continue
+		}
+
+		err := os.MkdirAll(filepath.Dir(path), 0755)
 		if err != nil {
 			return err
 		}
-		if !info.IsDir() {
-			folderSize += info.Size()
+
+		fileReader, err := file.Open()
+		if err != nil {
+			return err
 		}
-		return nil
-	})
+		defer fileReader.Close()
 
-	if err != nil {
-		return 0, err
+		targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+		defer targetFile.Close()
+
+		_, err = io.Copy(targetFile, fileReader)
+		if err != nil {
+			return err
+		}
 	}
-
-	return folderSize, nil
+	return nil
 }
